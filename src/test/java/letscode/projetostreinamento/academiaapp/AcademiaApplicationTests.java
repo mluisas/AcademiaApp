34b26786e74cec88dcea53c4820c71b7b0d68bac
F1685131 Breno Bezerra de Melo Alencar <breno.bezerraalencar@bb.com.br>
2022-08-05 14:06:57 -0300

    Cria estrutura base do projeto com Spring.

---
+++ src/test/java/letscode/projetostreinamento/academiaapp/AcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package letscode.projetostreinamento.academiaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
